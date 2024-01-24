2065dd667b1a2c612c5b2ba837ac59a87a32d4cc
María Belén <130631369+MariaBelenMartinezSanchez@users.noreply.github.com>
2024-01-23 19:37:52 -0600

    Add files via upload
    LandingPage_ODS4_MBMS.
    Es la 2 versión de la pagina, para Pacto Mundial.
    Se adapto el código con Bootstrap y se realizaron modificaciones para su correcto funcionamiento
    Miguel Cuellar (JAMéxico) y María Belén Martínez Sánchez
    23/01/2023
    
    http://yopuedoprogramar.org.mx/proyectos/d9P8XC3rzW/finalista_rework/index.html

---
+++ Version2_LandingPage_ODS4_MBMS/finalista-rework/js/application.js
@@ -0,0 +1,14 @@
+/* <script type="text/javascript">
+function imgSlider(anything){
+    document.querySelector('.ramas').src = anything
+}
+function changeCircleColor(color){
+    const circle = document.querySelector ('.circle');
+    circle.style.background = color;
+}
+function displayText(text){
+const textElement = document.getElementById('pilarTexto'); 
+textElement.textContent = text;
+}
+
+</script> */
